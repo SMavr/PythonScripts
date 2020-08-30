b304895d37f5277a62bf9254301725b5f1d80e63
Stamatis Mavrikis <stam.mavrikis@gmail.com>
2020-08-30 16:49:18 +0200

    Can open audio

--- openByAudio.py
+++ openByAudio.py
@@ -3,6 +3,17 @@ from selenium import webdriver
 import speech_recognition as sr
 
 
+def run():
+    """ Run """
+    chromedriver_path = r'chromedriver.exe'
+    os.environ['webdriver.chrome.driver'] = chromedriver_path
+    browser = webdriver.Chrome(chromedriver_path)
+    browser.get('https://www.youtube.com/results?search_query=rembetiko')
+    ##link_elem = browser.find_element_by_link_text('Οικονομία')
+    # link_elem.click()
+    # browser.quit()
+
+
 def recognizeAudio():
     # get audio from the microphone
     r = sr.Recognizer()
@@ -21,22 +32,12 @@ def recognizeAudio():
 
 
 def open():
-    print("Open!!@!")
+    run()
 
 switcher = {
     "open": open(),
 }
 
-
-def run():
-    """ Run """
-    chromedriver_path = r'chromedriver.exe'
-    os.environ['webdriver.chrome.driver'] = chromedriver_path
-    browser = webdriver.Chrome(chromedriver_path)
-    browser.get('https://www.youtube.com/results?search_query=rembetiko')
-    ##link_elem = browser.find_element_by_link_text('Οικονομία')
-    # link_elem.click()
-    # browser.quit()
 
 if __name__ == "__main__":
     recognizeAudio()
